8d8f1767ab007280410efc84045baa85fcabc975
qajeebolah <bisquadri77@gmail.com>
2023-03-16 11:20:06 -1100

    my project1

---
+++ javascript/jscode.js
@@ -0,0 +1,50 @@
+/*var years = 50;
+var age  =  'years old';
+ 
+alert   (years + age);*/
+
+/*function speakEnglish () {
+
+    alert ( 'i dont understand')
+
+}
+
+speakEnglish();*/
+
+/*function addSomething (num,str)  {
+    var  add = num + str ;
+    alert (add);
+
+}
+
+addSomething (45,' balls ');*/
+
+/*var food = 'eba';
+
+if  (food == 'apple') {
+
+    alert ('i ate apple');
+
+}
+
+else if (food == 'meat') {
+    alert ('i ate meat');
+}
+
+else {
+    alert ( 'sapa wa nle');
+}*/
+
+var i = 1;
+
+while (i < 5) {
+
+    document.write  ( 'a statement has run' );
+
+    i++;
+}
+
+/*for (var i = 1; i < 5; i++) {
+
+    document.write ('a stament has to run');
+}*/
