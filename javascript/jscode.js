cc5b1e01d540c52d077445e1f9577cdce2954af0
qajeebolah <bisquadri77@gmail.com>
2024-03-05 05:54:17 -1100

    my files

---
+++ javascript/jscode.js
@@ -0,0 +1,65 @@
+/*var years = 50;
+var age  =  'years old';
+ 
+alert   (years + age);*/
+
+/*function speakEnglish () {
+
+    alert ( 'i dont understand')
+
+}
+
+speakEnglish();*/
+
+/*function addSomething (num,str)  {
+    var  add = num + str ;
+    alert (add);
+
+}
+
+
+
+/*var i = 1;
+
+while (i < 5) {
+
+    document.write  ( 'a statement has run' );
+
+    i++;
+}*/
+
+/*for (var i = 1; i < 5; i++) {
+
+    document.write ('a stament has to run');
+}*/
+
+/*
+function changestyle() {
+    var text = document.getElementById("text").innerHTML = "green";
+
+
+}
+function changebackground(){
+    var para = document.getElementById("me").style.backgroundColor = "green";
+}
+
+function returnback(){
+    var para = document.getElementById("me").style.backgroundColor = "";
+}
+*/
+
+function replacetext() {
+
+    var thanks = document.getElementById("para").innerHTML = "asdupe";
+    
+    
+}
+/*
+function replacetext() {
+
+    var paragrah = document.getElementsByClassName('para');
+
+    var changeText = paragrah[0].innerHTML = "ogfddddfd";
+    var changeText = paragrah[1].innerHTML = "cleardfd";
+
+}*/
